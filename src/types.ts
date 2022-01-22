3bbb4f4feb1dbeda19c86bdec02143e58473c68d
takanamito <takanamito0928@gmail.com>
2022-01-22 16:14:44 +0900

    npx graphql-codegen --config ./config.yml

---
+++ src/types.ts
@@ -0,0 +1,60 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTodo: Todo;
+  createUser: User;
+};
+
+
+export type MutationCreateTodoArgs = {
+  input: NewTodo;
+};
+
+
+export type MutationCreateUserArgs = {
+  input: NewUser;
+};
+
+export type NewTodo = {
+  text: Scalars['String'];
+  userId: Scalars['String'];
+};
+
+export type NewUser = {
+  age: Scalars['Int'];
+  gender: Scalars['Int'];
+  name: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  todos: Array<Todo>;
+};
+
+export type Todo = {
+  __typename?: 'Todo';
+  id: Scalars['ID'];
+  text: Scalars['String'];
+  user: User;
+};
+
+export type User = {
+  __typename?: 'User';
+  age: Scalars['Int'];
+  gender: Scalars['Int'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
